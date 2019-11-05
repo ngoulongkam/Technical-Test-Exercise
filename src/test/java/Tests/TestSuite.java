ff0da5b7378f89c71379619f169e2d1aaa9f8873
ewanjonh <ewanjonh@gmail.com>
2019-11-05 22:20:38 +0000

    Added TestSuite to allow all tests to be ran at once

---
+++ src/test/java/Tests/TestSuite.java
@@ -0,0 +1,14 @@
+package Tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Create by Ewan.Hardingham on the 5th Nov 2019
+ */
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({Registration.class, SignIn.class, UserJourney.class})
+
+public class TestSuite {
+}
